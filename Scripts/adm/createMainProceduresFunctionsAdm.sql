3462850f334e4c907e54a9f0a132c319c375df08
Reyner Arias <Rey86581@gmail.com>
2020-07-19 15:12:51 -0600

    Create the main procedures of adm

---
+++ Scripts/adm/createMainProceduresFunctionsAdm.sql
@@ -0,0 +1,42 @@
+-- ADM procedures.
+-- Procedure to get a parameter with specific id to show it in the screen  
+DELIMITER $$
+CREATE PROCEDURE getParameter (pnIdParameter INT) 
+BEGIN
+	DECLARE vmenError VARCHAR(100);
+	SELECT id_parameter, name, value
+	FROM PARAMETER
+	WHERE id_parameter = IFNULL(pnIdParameter, id_parameter);
+END;
+
+-- Procedure to set a parameter with specific id and the new values wrote by the user  
+DELIMITER $$
+CREATE PROCEDURE setParameter (pnIdParameter INT, pcParameterName VARCHAR(45), pnParameterValue INT) 
+BEGIN
+	DECLARE vmenError VARCHAR(100);
+	UPDATE PARAMETER
+	SET name = pcParameterName,
+	value = pnParameterValue
+	WHERE id_parameter = pnIdParameter;
+	Commit;
+END;
+
+-- Procedure to delete a specific parameter  
+DELIMITER $$
+CREATE PROCEDURE deleteParameter (pnIdParameter INT) 
+BEGIN 
+	DECLARE vmenError VARCHAR(100);
+	DELETE FROM PARAMETER
+	WHERE id_parameter = pnIdParameter;
+	Commit;
+END;
+
+-- Procedure to insert a new parameter
+DELIMITER $$
+CREATE PROCEDURE insertParameter (pcParameterName VARCHAR(45), pnParameterValue INT)
+BEGIN
+	DECLARE vmenError VARCHAR(100);
+	INSERT INTO PARAMETER (name, value)
+	VALUES (pcParameterName, pnParameterValue);
+	Commit;
+END;
