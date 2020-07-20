dd45c43b2626d171b921efb43b30cccbe2c61fde
Kenneth Ibarra <>
2020-07-20 16:08:19 -0600

    Create the main window for the administrator

---
+++ HouseOfTrade/src/front_end/MainAdmin.java
@@ -0,0 +1,133 @@
+package front_end;
+
+public class MainAdmin extends javax.swing.JDialog {
+
+    public MainAdmin(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnRegisterProduct = new javax.swing.JButton();
+        btnViewProducts = new javax.swing.JButton();
+        btnEditCatalogs = new javax.swing.JButton();
+        btnQueries = new javax.swing.JButton();
+        btnStatistics = new javax.swing.JButton();
+        btnExit = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        btnRegisterProduct.setText("Register Product");
+        btnRegisterProduct.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegisterProductActionPerformed(evt);
+            }
+        });
+
+        btnViewProducts.setText("View Products");
+        btnViewProducts.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnViewProductsActionPerformed(evt);
+            }
+        });
+
+        btnEditCatalogs.setText("Edit Catalogs");
+        btnEditCatalogs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditCatalogsActionPerformed(evt);
+            }
+        });
+
+        btnQueries.setText("Queries");
+        btnQueries.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnQueriesActionPerformed(evt);
+            }
+        });
+
+        btnStatistics.setText("Statistics");
+        btnStatistics.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStatisticsActionPerformed(evt);
+            }
+        });
+
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(142, 142, 142)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(btnRegisterProduct)
+                    .addComponent(btnEditCatalogs)
+                    .addComponent(btnViewProducts)
+                    .addComponent(btnQueries)
+                    .addComponent(btnStatistics)
+                    .addComponent(btnExit))
+                .addContainerGap(145, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnRegisterProduct)
+                .addGap(18, 18, 18)
+                .addComponent(btnViewProducts)
+                .addGap(18, 18, 18)
+                .addComponent(btnEditCatalogs)
+                .addGap(18, 18, 18)
+                .addComponent(btnQueries)
+                .addGap(18, 18, 18)
+                .addComponent(btnStatistics)
+                .addGap(18, 18, 18)
+                .addComponent(btnExit)
+                .addContainerGap(61, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnRegisterProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegisterProductActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegisterProductActionPerformed
+
+    private void btnViewProductsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViewProductsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnViewProductsActionPerformed
+
+    private void btnEditCatalogsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditCatalogsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEditCatalogsActionPerformed
+
+    private void btnQueriesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnQueriesActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnQueriesActionPerformed
+
+    private void btnStatisticsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStatisticsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnStatisticsActionPerformed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnEditCatalogs;
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnQueries;
+    private javax.swing.JButton btnRegisterProduct;
+    private javax.swing.JButton btnStatistics;
+    private javax.swing.JButton btnViewProducts;
+    // End of variables declaration//GEN-END:variables
+}
