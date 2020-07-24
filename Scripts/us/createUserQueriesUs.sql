cef5f0e69ba0a376cdbd8e2993f5c89458a51513
Davenb42 <66222338+Davenb42@users.noreply.github.com>
2020-07-24 14:35:43 -0600

    Created some windows for the Queries

--- Scripts/us/createUserQueriesUs.sql
+++ Scripts/us/createUserQueriesUs.sql
@@ -7,7 +7,7 @@ BEGIN
     DECLARE EXIT HANDLER FOR 1118  SELECT 'Row size too large' Message;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQLException encountered' Message; 
     DECLARE EXIT HANDLER FOR SQLSTATE '23000' SELECT 'SQLSTATE 23000' ErrorCode;
-    Select p.name, uv.username viewed_by from user u
+    Select p.name, uv.username viewed_by, up.date_creation viewed from user u
     inner join sh.product p on p.UsernameSalesman = u.username
     inner join userviewsproduct up on up.ID_Product = p.ID_Product
     inner join user uv on uv.Username = up.Username
@@ -36,10 +36,28 @@ BEGIN
     DECLARE EXIT HANDLER FOR 1118  SELECT 'Row size too large' Message;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQLException encountered' Message; 
     DECLARE EXIT HANDLER FOR SQLSTATE '23000' SELECT 'SQLSTATE 23000' ErrorCode;
-    select pu.ID_Purchase, sum(pr.price) Total_Price from user u
+    select pu.ID_Purchase ID_Purchase, pm.Name, sum(pr.price) Total_Price from user u
     inner join sh.purchase pu on pu.Username_Customer = u.Username
     inner join sh.productxpurchase pp on pp.ID_Purchase = pu.ID_Purchase
     inner join sh.product pr on pr.ID_Product = pp.ID_Product
+    inner join paymentmethod pm on pm.ID_PaymentMethod = pu.ID_PaymentMethod
     where u.Username = psUsername and Total_Price >= 1000 and p.ID_category = IFNULL(pnID_Category, p.ID_category);
 END$$
 
+-- Procedure that gets the list of the most recent purchases
+DELIMITER $$
+CREATE PROCEDURE getPurhaseHistory (month int, psUsername varchar(45)) 
+BEGIN
+	DECLARE EXIT HANDLER FOR 1062 SELECT 'Duplicate keys error encountered' Message; 
+    DECLARE EXIT HANDLER FOR 1118  SELECT 'Row size too large' Message;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION SELECT 'SQLException encountered' Message; 
+    DECLARE EXIT HANDLER FOR SQLSTATE '23000' SELECT 'SQLSTATE 23000' ErrorCode;
+	-- Corregir calculo de meses
+	Select pu.ID_Purchase ID_Purchase, pu.Date Date, pm.Name,  sum(pr.price) Total_Price
+    from user u
+    inner join sh.purchase pu on pu.Username_Customer = u.Username
+    inner join sh.productxpurchase pp on pp.ID_Purchase = pu.ID_Purchase
+    inner join sh.product pr on pr.ID_Product = pp.ID_Product
+    inner join paymentmethod pm on pm.ID_PaymentMethod = pu.ID_PaymentMethod
+    where u.username = psUsername and month(date) = month;
+END$$
